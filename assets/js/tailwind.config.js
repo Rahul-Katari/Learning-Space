0f65dcc64b566096f152b3d1ccfd2f24ebda38c2
Rahul Katari <rahulkatari99@gmail.com>
2025-03-18 11:05:43 +0530

    added news and news details pages

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        container: {
+            center: true,
+            padding: '1rem',
+            screens: {
+                sm: "100%",
+                md: "100%",
+                lg: "1024px",
+                xl: "1200px",
+                "2xl": "1400px",
+            }
+        }
+    }
+}
